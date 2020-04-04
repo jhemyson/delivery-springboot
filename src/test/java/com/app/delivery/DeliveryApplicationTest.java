2d18283f80247520677d56f621ac136a69def29a
Jhemyson <jhemyson_@hotmail.com>
2020-04-04 08:41:32 -0300

    init project

---
+++ src/test/java/com/app/delivery/DeliveryApplicationTest.java
@@ -0,0 +1,14 @@
+package com.app.delivery;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DeliveryApplicationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
